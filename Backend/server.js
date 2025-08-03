cf3643cbb2c53fd4367719d1f7e76d0951fac078
Andreicomia11-12 <andreicomia7@gmail.com>
2025-08-03 17:13:18 +0800

    not that much changes

--- Backend/server.js
+++ Backend/server.js
@@ -1,45 +1,41 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const cors = require('cors');
-const userRoutes = require('./routes/userRoutes');
-const authRoutes = require('./routes/authRoutes');
-const lgbtqRoutes = require('./routes/lgbtqProfileRoutes');
-const path = require('path');
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const userRoutes = require("./routes/userRoutes");
+const authRoutes = require("./routes/authRoutes");
+const lgbtqRoutes = require("./routes/lgbtqProfileRoutes");
+const path = require("path");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
 
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
-app.use('/api/users', userRoutes);
-app.use('/api/auth', authRoutes);
-app.use('/api/kkprofiling', require('./routes/kkProfileRoutes'));
-app.use('/api/formstatus', require('./routes/formStatusRoutes')); // Optional
+app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/kkprofiling", require("./routes/kkProfileRoutes"));
+app.use("/api/formstatus", require("./routes/formStatusRoutes")); // Optional
 
-app.use(express.static('public'));
-app.use('/api/lgbtqprofiling', lgbtqRoutes);
-
+app.use(express.static("public"));
+app.use("/api/lgbtqprofiling", lgbtqRoutes);
 
 const PORT = process.env.PORT || 5000;
 
 // MongoDB connection
-mongoose.connect(process.env.MONGO_URI)
-    .then(() => {
-      console.log('MongoDB connected');
-      app.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`);
-      });
-    })
-    .catch((err) => {
-      console.error(err);
-      process.exit(1);
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected");
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
     });
-
-
-
+  })
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
