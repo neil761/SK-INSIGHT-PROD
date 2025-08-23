e09119ac3df0fad24b24b0ccb598d1ef6df2294d
neil761 <alvarezneilandrew486@gmail.com>
2025-08-23 14:03:02 +0800

    log

--- Frontend/js/admin-log.js
+++ Frontend/js/admin-log.js
@@ -21,7 +21,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const password = passwordField.value.trim();
 
         try {
-            const res = await fetch('http://localhost:5000/api/auth/login', {
+            const res = await fetch('http://localhost:5000/api/auth/admin/login', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ username, password })
