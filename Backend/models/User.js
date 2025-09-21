432920e6f1c42e897f5c914a25509de3e0ebbf02
neil761 <alvarezneilandrew486@gmail.com>
2025-09-21 21:11:48 +0800

    user

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -26,6 +26,8 @@ const userSchema = new mongoose.Schema({
     type: String,
     trim: true,
   },
+  gender: { type: String },
+  civilStatus: { type: String },
 });
 
 // Hash password before saving
